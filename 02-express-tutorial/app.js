c1841f5928907458e88bad288a187ed948f1024d
Aadityakiran S <aadityakirans@gmail.com>
2023-02-06 11:13:19 +0530

    Made a basic workin g web page with just http module

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,38 @@
 const http = require("http");
-// const {readFileSync} = require('fs');
+const { readFileSync } = require("fs");
 
 //Get all files
-// const homePage = readFileSync('./index.html');
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
   res.writeHead(200, { "content-type": "text/html" });
+  console.log(url);
   //This is the home page
   if (url === "/") {
-    res.write("<h1>Welcome to our home page</h1>");
+    res.write(homePage);
   }
   //About page
   else if (url === "/about") {
     res.write("<h1>This is our about page</h1>");
+  } 
+  //styles-css
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
+  }
+  //logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
+  }
+  //logic for the site
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
   }
   //Return 404 error
   else {
--- 02-express-tutorial/final/02-http-app.js
+++ 02-express-tutorial/final/02-http-app.js
@@ -1,52 +1,45 @@
-const http = require('http')
-const { readFileSync } = require('fs')
+const http = require("http");
+const { readFileSync } = require("fs");
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeImage = readFileSync('./navbar-app/logo.svg')
-const homeLogic = readFileSync('./navbar-app/browser-app.js')
+//Get all files
+const homePage = readFileSync("./navbar-app/index.html");
+const homeStyles = readFileSync("./navbar-app/styles.css");
+const homeImage = readFileSync("./navbar-app/logo.svg");
+const homeLogic = readFileSync("./navbar-app/browser-app.js");
 
 const server = http.createServer((req, res) => {
-  // console.log(req.method)
-  const url = req.url
-  console.log(url)
-  // home page
-  if (url === '/') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write(homePage)
-    res.end()
+  const url = req.url;
+  res.writeHead(200, { "content-type": "text/html" });
+  console.log(url);
+  //This is the home page
+  if (url === "/") {
+    res.write(homePage);
   }
-  // about page
-  else if (url === '/about') {
-    res.writeHead(200, { 'content-type': 'text/html' })
-    res.write('<h1>about page</h1>')
-    res.end()
+  //About page
+  else if (url === "/about") {
+    res.write("<h1>This is our about page</h1>");
+  } 
+  //styles-css
+  else if (url === "/styles.css") {
+    res.writeHead(200, { "content-type": "text/css" });
+    res.write(homeStyles);
   }
-  // styles
-  else if (url === '/styles.css') {
-    res.writeHead(200, { 'content-type': 'text/css' })
-    res.write(homeStyles)
-    res.end()
+  //logo
+  else if (url === "/logo.svg") {
+    res.writeHead(200, { "content-type": "image/svg+xml" });
+    res.write(homeImage);
   }
-  // image/logo
-  else if (url === '/logo.svg') {
-    res.writeHead(200, { 'content-type': 'image/svg+xml' })
-    res.write(homeImage)
-    res.end()
+  //logic for the site
+  else if (url === "/browser-app.js") {
+    res.writeHead(200, { "content-type": "text/javascript" });
+    res.write(homeLogic);
   }
-  // logic
-  else if (url === '/browser-app.js') {
-    res.writeHead(200, { 'content-type': 'text/javascript' })
-    res.write(homeLogic)
-    res.end()
-  }
-  // 404
+  //Return 404 error
   else {
-    res.writeHead(404, { 'content-type': 'text/html' })
-    res.write('<h1>page not found</h1>')
-    res.end()
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Bad Request!</h1>");
   }
-})
+  res.end();
+});
 
-server.listen(5000)
+server.listen(3000);
