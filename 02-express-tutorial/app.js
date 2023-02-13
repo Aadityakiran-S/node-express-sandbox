fa15b5ba1dc6cc978407caa8ad5dee40a6c9f907
AadityakiranS <aadityakirans@gmail.com>
2023-02-13 11:10:33 +0530

    Learnt about API methods using express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,38 +1,91 @@
 const express = require('express');
-const morgan = require('morgan');
-const logger = require('./logger.js');
-const authorize = require('./authorize.js');
-
 const app = express();
+let { people } = require('./data.js');
 
+//Static assets setup to get site up and running
+app.use(express.static('./methods-public'));
 
-// app.use('/api', [authorize, logger]); //app.use() sets up middleware
-app.use(morgan('tiny')); 
-//Make sure you call it before you call get otherwise won't work
-//NodeJS, order is important
-//MIDDLEWARE FIRST  
-// app.use(express.static('./public'));
+//Middleware to parse from data in traditional form (x-www-form-urlencoded)
+var parse = express.urlencoded({ extended: false });
+//Middleware to parse if sending post req through JSON
+var jsonParser = express.json();
 
-//req => middleware => res
-app.get('/', logger, (req, res) => {
-    // logger(); //Do not call this function here, it's not required. CRAZY syntax, I know
-    res.send('<h1>Home Page</h1>');
-});
-
-//Need to add logger since it only automatically adds to /api paths
-app.get('/about', logger, (req, res) => {
-    res.send('<h1>About Page</h1>');
+app.get('/api/people', (req, res) => {
+    res.status(200).json({ success: true, data: people });
 })
 
-//logger is applied to all paths that come after '/api' using .use() func
-app.get('/api/products', (req, res) => {
-    res.send('<h1>Products</h1>');
+//This is for JavaScript form (javascript.html)
+//Data is passed in the form of JSON so we need jsonParser
+app.post('/api/people', jsonParser, (req, res) => {
+    console.log(req.body);
+    const { name } = req.body;
+    if (!name) {
+        return res.status(401).json({ sucess: false, message: `Please provide credentials` });
+    }
+    res.status(201).json({ success: true, message: `Welcome ${name}` });
 })
 
-app.get('/api/items', (req, res) => {
-    res.send(`<h1>Items with user ${JSON.stringify(req.user.name)}</h1>`);
+//Created a seperate method to test through postman
+app.post('/api/postman/people', jsonParser, (req, res) => {
+    const {name} = req.body;
+    console.log(name);
+    if (!name) {
+        return res.status(401).json({ success: false, message: "Please provide credentials" });
+    }
+    //Adding our person to the list and showing in output
+    var person = {id: people[people.length -1].id + 1, name: name};
+    return res.status(200).json({ success: true, 
+        data: [...people, person]
+    });
+})
+
+//Here we send the ID in url and the data to change in JSON
+app.put('/api/people/:id', jsonParser, (req, res) =>{
+    const {id} = req.params; //ID passed through URL
+    const {name} = req.body; //value passed through body
+
+    //Checking if person exists
+    const person = people.find((person) => person.id === Number(id));
+    if(!person){
+        return res.status(400).json({success: false, message: `Person with Id ${id} DNE`});
+    }
+
+    const newPeople = people.map((person) =>{
+        if(person.id === Number(id)){
+            person.name = name;
+        }
+        return person;
+    })
+    res.status(200).json({success: true, data: newPeople});
+})
+
+//Same thing as above, ID from URL and data from JSON
+app.delete('/api/people/:id', jsonParser, (req, res) =>{
+    const {id} = req.params;
+
+    const person = people.find((person) => person.id === Number(id));
+    if(!person){
+        return res.send(404).json({success: false, message: `Perons with ID ${id} DNE`});
+    }
+
+    const newPeople = people.filter((person) => person.id !== Number(id));
+
+    res.status(200).json({success: true, data: newPeople});
+})
+
+//This is for traditional form (index.html)
+//Data is passed in x-www-form-urlencoded so only need parse
+app.post('/login', parse, (req, res) => {
+    console.log(req.body);
+    const { name } = req.body;
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`);
+    }
+    else {
+        return res.status(401).send(`Please provide credentials`);
+    }
 })
 
 app.listen(3000, () => {
-    console.log('Server is listening on port 3000...');
+    console.log('Server is listening on port 3000....');
 })
--- 02-express-tutorial/final/11-methods.js
+++ 02-express-tutorial/final/11-methods.js
@@ -1,80 +1,91 @@
-const express = require('express')
-const app = express()
-let { people } = require('./data')
+const express = require('express');
+const app = express();
+let { people } = require('./data.js');
 
-// static assets
-app.use(express.static('./methods-public'))
-// parse form data
-app.use(express.urlencoded({ extended: false }))
-// parse json
-app.use(express.json())
+//Static assets setup to get site up and running
+app.use(express.static('./methods-public'));
+
+//Middleware to parse from data in traditional form (x-www-form-urlencoded)
+var parse = express.urlencoded({ extended: false });
+//Middleware to parse if sending post req through JSON
+var jsonParser = express.json();
 
 app.get('/api/people', (req, res) => {
-  res.status(200).json({ success: true, data: people })
+    res.status(200).json({ success: true, data: people });
 })
 
-app.post('/api/people', (req, res) => {
-  const { name } = req.body
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: 'please provide name value' })
-  }
-  res.status(201).json({ success: true, person: name })
-})
-
-app.post('/api/postman/people', (req, res) => {
-  const { name } = req.body
-  if (!name) {
-    return res
-      .status(400)
-      .json({ success: false, msg: 'please provide name value' })
-  }
-  res.status(201).json({ success: true, data: [...people, name] })
-})
-
-app.post('/login', (req, res) => {
-  const { name } = req.body
-  if (name) {
-    return res.status(200).send(`Welcome ${name}`)
-  }
-
-  res.status(401).send('Please Provide Credentials')
-})
-
-app.put('/api/people/:id', (req, res) => {
-  const { id } = req.params
-  const { name } = req.body
-
-  const person = people.find((person) => person.id === Number(id))
-
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${id}` })
-  }
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name
+//This is for JavaScript form (javascript.html)
+//Data is passed in the form of JSON so we need jsonParser
+app.post('/api/people', jsonParser, (req, res) => {
+    console.log(req.body);
+    const { name } = req.body;
+    if (!name) {
+        return res.status(401).json({ sucess: false, message: `Please provide credentials` });
     }
-    return person
-  })
-  res.status(200).json({ success: true, data: newPeople })
+    res.status(201).json({ success: true, message: `Welcome ${name}` });
 })
 
-app.delete('/api/people/:id', (req, res) => {
-  const person = people.find((person) => person.id === Number(req.params.id))
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `no person with id ${req.params.id}` })
-  }
-  const newPeople = people.filter(
-    (person) => person.id !== Number(req.params.id)
-  )
-  return res.status(200).json({ success: true, data: newPeople })
+//Created a seperate method to test through postman
+app.post('/api/postman/people', jsonParser, (req, res) => {
+    const {name} = req.body;
+    console.log(name);
+    if (!name) {
+        return res.status(401).json({ success: false, message: "Please provide credentials" });
+    }
+    //Adding our person to the list and showing in output
+    var person = {id: people[people.length -1].id + 1, name: name};
+    return res.status(200).json({ success: true, 
+        data: [...people, person]
+    });
 })
 
-app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+//Here we send the ID in url and the data to change in JSON
+app.put('/api/people/:id', jsonParser, (req, res) =>{
+    const {id} = req.params; //ID passed through URL
+    const {name} = req.body; //value passed through body
+
+    //Checking if person exists
+    const person = people.find((person) => person.id === Number(id));
+    if(!person){
+        return res.status(400).json({success: false, message: `Person with Id ${id} DNE`});
+    }
+
+    const newPeople = people.map((person) =>{
+        if(person.id === Number(id)){
+            person.name = name;
+        }
+        return person;
+    })
+    res.status(200).json({success: true, data: newPeople});
+})
+
+//Same thing as above, ID from URL and data from JSON
+app.delete('/api/people/:id', jsonParser, (req, res) =>{
+    const {id} = req.params;
+
+    const person = people.find((person) => person.id === Number(id));
+    if(!person){
+        return res.send(404).json({success: false, message: `Perons with ID ${id} DNE`});
+    }
+
+    const newPeople = people.filter((person) => person.id !== Number(id));
+
+    res.status(200).json({success: true, data: newPeople});
+})
+
+//This is for traditional form (index.html)
+//Data is passed in x-www-form-urlencoded so only need parse
+app.post('/login', parse, (req, res) => {
+    console.log(req.body);
+    const { name } = req.body;
+    if (name) {
+        return res.status(200).send(`Welcome ${name}`);
+    }
+    else {
+        return res.status(401).send(`Please provide credentials`);
+    }
+})
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000....');
 })
