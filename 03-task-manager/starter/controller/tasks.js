46410ac6f9b68da64d8d9e3ae8467137ca5aa916
AadityakiranS <aadityakirans@gmail.com>
2023-03-08 16:02:23 +0530

    Implemented more thurough API response for getAllTasks

--- 03-task-manager/starter/controller/tasks.js
+++ 03-task-manager/starter/controller/tasks.js
@@ -1,18 +1,22 @@
 const Task = require('../models/Task.js');
-const Task_DBSchema = require('../models/Task.js');
 
 const getAllTasks = async (req, res) => {
     try {
-        let task = await Task_DBSchema.find({});
-        res.status(200).json({ task });
+        let tasks = await Task.find({});
+        // res.status(200).json({ task });
+        res
+            .status(200)
+            .json({ success: true, data: { task: tasks }, count: tasks.length });
     } catch (error) {
-        res.status(500).json({ error });
+        res
+            .status(500)
+            .json({ success: false, data: { error } });
     }
 }
 
 const createTask = async (req, res) => {
     try {
-        const task = await Task_DBSchema.create(req.body);
+        const task = await Task.create(req.body);
         res.status(200).json({ task });
     } catch (error) {
         res.status(500).json({ message: error });
@@ -22,7 +26,7 @@ const createTask = async (req, res) => {
 const getTask = async (req, res) => {
     try {
         const { id: taskID } = req.params;
-        const task = await Task_DBSchema.findOne({ _id: taskID });
+        const task = await Task.findOne({ _id: taskID });
         if (!task) {
             return res.status(404).json({ message: `Task with ID ${taskID} DNE` });
         }
@@ -36,7 +40,7 @@ const updateTask = async (req, res) => {
     try {
         const { id: taskID } = req.params;
         //Options object should be set to run validators to make sure updation follows validtion rules earlier set and also new: true sends back response as updated value not old one
-        const task = await Task_DBSchema.findOneAndUpdate({ _id: taskID }, req.body, {
+        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
             new: true, runValidators: true
         });
         if (!task) {
@@ -52,7 +56,7 @@ const updateTask = async (req, res) => {
 const editTask = async (req, res) => {
     try {
         const { id: taskID } = req.params;
-        const task = await Task_DBSchema.findOneAndUpdate({ _id: taskID }, req.body, {
+        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
             new: true, runValidators: true, overwrite: true,
             //Mongoose by default has the same behaviour for PUT and PATCH but setting overwrite: will differentiate put from patch by causing overwrite for the operation.
         })
@@ -71,7 +75,7 @@ const editTask = async (req, res) => {
 const deleteTask = async (req, res) => {
     try {
         const { id: taskID } = req.params;
-        const task = await Task_DBSchema.findOneAndDelete({ _id: taskID });
+        const task = await Task.findOneAndDelete({ _id: taskID });
         if (!task) {
             return res.status(404).json({ message: `Task with ID: ${taskID} DNE` });
         }
