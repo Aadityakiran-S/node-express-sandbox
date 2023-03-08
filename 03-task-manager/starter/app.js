8f5e8ab6479f20cf50fdd1cfa9738f54a81b8053
AadityakiranS <aadityakirans@gmail.com>
2023-03-08 16:32:53 +0530

    Added async wrapper and also not found response for all invalid paths

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks.js');
 const connectDB = require('./db/connect.js');
+const notFound = require('./middleware/not-found.js');
 require('dotenv').config();
 
 const port = 3000;
+
+//#region Middleware
 
 //Setting up static files
 app.use(express.static('./public'));
@@ -12,8 +15,16 @@ app.use(express.static('./public'));
 //JSON parser middleware to converte from JSON for all APIs
 app.use(express.json());
 
+
+
+//#endregion
+
 //Using this route for the rest of the APIs: Aso a middleware
 app.use('/api/v1/tasks', tasks);
+
+//Route for for any path other than already defined. Set this up only AFTER ALL OTHER PATHS ARE DEFINED as above
+app.use(notFound);
+
 
 //Establishing connection to MongoDB
 const start = async (mongoURI) => {
@@ -29,4 +40,3 @@ const start = async (mongoURI) => {
 }
 
 start(process.env.MONGO_URI);
-
--- 03-task-manager/starter/controller/tasks.js
+++ 03-task-manager/starter/controller/tasks.js
@@ -1,89 +1,60 @@
 const Task = require('../models/Task.js');
+const asyncWrapper = require('../middleware/async.js');
 
-const getAllTasks = async (req, res) => {
-    try {
-        let tasks = await Task.find({});
-        // res.status(200).json({ task });
-        res
-            .status(200)
-            .json({ success: true, data: { task: tasks }, count: tasks.length });
-    } catch (error) {
-        res
-            .status(500)
-            .json({ success: false, data: { error } });
-    }
-}
+const getAllTasks = asyncWrapper(async (req, res) => {
+    let tasks = await Task.find({});
+    res
+        .status(200)
+        .json({ success: true, data: { task: tasks }, count: tasks.length });
+})
 
-const createTask = async (req, res) => {
-    try {
-        const task = await Task.create(req.body);
-        res.status(200).json({ task });
-    } catch (error) {
-        res.status(500).json({ message: error });
-    }
-}
+const createTask = asyncWrapper(async (req, res) => {
+    const task = await Task.create(req.body);
+    res.status(200).json({ task });
+})
 
-const getTask = async (req, res) => {
-    try {
-        const { id: taskID } = req.params;
-        const task = await Task.findOne({ _id: taskID });
-        if (!task) {
-            return res.status(404).json({ message: `Task with ID ${taskID} DNE` });
-        }
-        res.status(200).json({ task });
-    } catch (error) {
-        res.status(500).json({ error });
+const getTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+        return res.status(404).json({ message: `Task with ID ${taskID} DNE` });
     }
-}
+    res.status(200).json({ task });
+})
 
-const updateTask = async (req, res) => {
-    try {
-        const { id: taskID } = req.params;
-        //Options object should be set to run validators to make sure updation follows validtion rules earlier set and also new: true sends back response as updated value not old one
-        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-            new: true, runValidators: true
-        });
-        if (!task) {
-            return res.status(404).json({ msg: `Task with ID: ${taskID} DNE` });
-        }
-        res.status(200).json({ id: taskID, body: req.body });
-    } catch (error) {
-        res.status(500).json({ error });
+const updateTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params;
+    //Options object should be set to run validators to make sure updation follows validtion rules earlier set and also new: true sends back response as updated value not old one
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+        new: true, runValidators: true
+    });
+    if (!task) {
+        return res.status(404).json({ msg: `Task with ID: ${taskID} DNE` });
     }
-}
+    res.status(200).json({ id: taskID, body: req.body });
+})
 
 //This API resets all the other fileds to default values.
-const editTask = async (req, res) => {
-    try {
-        const { id: taskID } = req.params;
-        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
-            new: true, runValidators: true, overwrite: true,
-            //Mongoose by default has the same behaviour for PUT and PATCH but setting overwrite: will differentiate put from patch by causing overwrite for the operation.
-        })
-
-        if (!task) {
-            return res.status(404).json({ msg: `No task with ID ${taskID}` });
-        }
-
-        res.status(200).json({ task });
+const editTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+        new: true, runValidators: true, overwrite: true,
+        //Mongoose by default has the same behaviour for PUT and PATCH but setting overwrite: will differentiate put from patch by causing overwrite for the operation.
+    })
+    if (!task) {
+        return res.status(404).json({ msg: `No task with ID ${taskID}` });
     }
-    catch (error) {
-        res.status(500).json({ msg: error });
-    }
-}
+    res.status(200).json({ task });
+})
 
-const deleteTask = async (req, res) => {
-    try {
-        const { id: taskID } = req.params;
-        const task = await Task.findOneAndDelete({ _id: taskID });
-        if (!task) {
-            return res.status(404).json({ message: `Task with ID: ${taskID} DNE` });
-        }
-        res.status(200).json({ task });
-    } catch (error) {
-        res.status(500).json({ error });
+const deleteTask = asyncWrapper(async (req, res) => {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+        return res.status(404).json({ message: `Task with ID: ${taskID} DNE` });
     }
-}
+    res.status(200).json({ task });
+})
 
 module.exports = {
     getAllTasks,
