a889629db0b7a28f42668885ee04ad288b3aed6e
Aadityakiran S <aadityakirans@gmail.com>
2023-11-15 11:11:38 +0530

    Initial setup of the store-api project

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,38 +1,30 @@
 require('dotenv').config();
-// async errors
+//async errors
 
 const express = require('express');
 const app = express();
 
-//Connect to the DB
-const connectDB = require('./db/connect.js');
-const productsRoute = require('./routes/products.js');
-
-const notFoundMiddleWare = require('./middleware/not-found.js');
-const errorMiddleware = require('./middleware/error-handler.js');;
+const notFoundMiddleware = require('./middleware/not-found.js');
+const errorHandlingMiddleware = require('./middleware/error-handler.js');
 
 //middleware
 app.use(express.json());
 
 //routes
+
 app.get('/', (req, res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
-})
+});
 
-app.use('/api/v1/products', productsRoute);
-
-//product routes
-
-app.use(notFoundMiddleWare);
-app.use(errorMiddleware);
+app.use(errorHandlingMiddleware); app.use(notFoundMiddleware);
 
 const port = process.env.PORT || 3000;
+
 const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI);
-        app.listen(port, console.log(`Server is listening on port ${port}`));
-    }
-    catch (error) {
+        //Connect to DB
+        app.listen(port, console.log(`Server listneing on port ${port}..`));
+    } catch (error) {
         console.log(error);
     }
 }
